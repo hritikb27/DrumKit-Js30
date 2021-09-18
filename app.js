60c86651b69b0d87f790a316c8505fc66b8b2259
hritikb27 <hritikb90@gmail.com>
2021-09-18 19:59:24 +0530

    Added Sounds, Completed!

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const allKeys = document.querySelectorAll('.hover-eff');
 
 Array.from(allKeys, (e)=>{
-    console.log(e.getAttribute('data-key'));
+    e.addEventListener('click', ()=>{
+        const key = e.getAttribute('data-key');
+        const getInstr = document.querySelector(`audio[data-key="${key}"]`);
+        getInstr.currentTime =0;
+        getInstr.play();
+    })
+})
+
+window.addEventListener('keydown', (e)=>{
+    const key = e.keyCode;
+    const getInstr = document.querySelector(`audio[data-key="${key}"]`);
+
+    if(!getInstr){
+        return;
+    }
+    getInstr.currentTime = 0;
+    getInstr.play();
 })
